e3124a10b8191072b72784995d308488ecad126b
IWriteThings <IWriteThings@users.noreply.github.com>
2016-08-01 14:49:51 -0700

    Style Cleanup

--- index.php
+++ index.php
@@ -5,23 +5,16 @@
 		<meta charset="utf-8">
 		<style>
 			@import url(https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz);
-			@import url(https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic);
+			@import url(https://fonts.googleapis.com/css?family=Droid+Sans:400,700,400italic);
 			@import url(https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700,400italic);
-
-			body { font-family: 'Droid Serif'; }
-			h1, h2, h3 {
-				font-family: 'Yanone Kaffeesatz';
-				font-weight: normal;
-			}
+			body { font-family: 'Droid Sans'; }
+			h1, h2, h3 { font-family: 'Yanone Kaffeesatz'; font-weight: normal; }
 			.remark-code, .remark-inline-code { font-family: 'Ubuntu Mono'; }
 		</style>
 	</head>
 	<body>
 	<?php include( 'slides.php' ); ?>
-	<script src="https://gnab.github.io/remark/downloads/remark-latest.min.js">
-	</script>
-	<script>
-		var slideshow = remark.create();
-	</script>
+	<script src="https://gnab.github.io/remark/downloads/remark-latest.min.js"></script>
+	<script> var slideshow = remark.create(); </script>
 	</body>
 </html>
